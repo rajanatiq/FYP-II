4451b5f62a1d95a4c753be56dc4dc4526b1f2086
MalikAwais09 <awaisattari047@gmail.com>
2026-02-16 11:40:20 +0500

    create exam

--- API/Schemas/ExamCreate.py
+++ API/Schemas/ExamCreate.py
@@ -1,11 +1,11 @@
-from datetime import date
+from datetime import datetime
 from pydantic import BaseModel
 
 class ExamCreate(BaseModel):
     A_ID: int
     TITLE: str
     TOTAL_QUESTIONS: int
-    E_DATE: date
+    E_DATE: datetime
     START_TIME: str
     END_TIME: str
     E_TYPE: str
