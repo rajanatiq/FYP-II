afa2091a4a96a6240d63cf54856da8caa46c841e
rajanatiq <mq2027474@gmail.com>
2026-05-14 22:20:28 +0500

    Update CourseEnrollment.py

--- API/Models/CourseEnrollment.py
+++ API/Models/CourseEnrollment.py
@@ -1,4 +1,4 @@
-from db import Base
+from API.db import Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Date
 from datetime import datetime  
 from sqlalchemy.orm import relationship
